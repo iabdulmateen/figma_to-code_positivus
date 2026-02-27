1fd47a95435943aeec7a794a0aa8f5b7d8780470
abdul mateen <innovatecode121@gmail.com>
2026-02-27 13:39:33 -0800

    Completed landing page structure including Hero Services Team and Testimonials sections

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navLinks = document.querySelector('.nav-links');
 menuBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+    header.addEventListener('click', () => {
+        // Kisi aur item ko band karne ke liye (Optional)
+        // accordionItems.forEach(i => i.classList.remove('active'));
+        
+        item.classList.toggle('active');
+    });
+});
